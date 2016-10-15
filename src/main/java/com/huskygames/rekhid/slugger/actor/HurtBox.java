aa95d3d13c00f4f4c807ae7e00dc4b1baf14810d
Tory Taggart <tltaggar@mtu.edu>
2016-10-15 18:31:30 -0400

    added hurtbox class

--- src/main/java/com/huskygames/rekhid/slugger/actor/HurtBox.java
+++ src/main/java/com/huskygames/rekhid/slugger/actor/HurtBox.java
@@ -1,7 +1,19 @@
 package com.huskygames.rekhid.slugger.actor;
 
-/**
- * Created by tltaggar on 10/15/16.
- */
-public class HurtBox {
+import com.huskygames.rekhid.slugger.util.DoublePair;
+
+public class HurtBox extends ActorCircle {
+    private DoublePair effect;
+    private int damage;
+    private int lifetime;
+
+    public HurtBox(DoublePair offset, Actor parent, double r, DoublePair hit, int damage){
+        super(offset, parent, r);
+        effect = hit;
+        this.damage = damage;
+    }
+
+    public DoublePair getEffect() {
+        return effect;
+    }
 }
