b253addca3a41e3801129b19f8782cdef8062aba
Alex <aborman7@gmail.com>
2017-01-21 16:51:26 -0500

    - Removed extra imports

--- src/main/java/com/huskygames/rekhid/MainMenu.java
+++ src/main/java/com/huskygames/rekhid/MainMenu.java
@@ -1,36 +1,17 @@
 package com.huskygames.rekhid;
 
-import com.huskygames.rekhid.slugger.input.ButtonEvent;
 import com.huskygames.rekhid.slugger.menu.Menu;
-import com.huskygames.rekhid.slugger.menu.MenuItem;
-import net.java.games.input.Controller;
-import net.java.games.input.ControllerEnvironment;
+
 import com.huskygames.rekhid.slugger.input.ControllerInput;
 import com.huskygames.rekhid.slugger.menu.TextButton;
 import com.huskygames.rekhid.slugger.resource.LoadedImage;
 import com.huskygames.rekhid.slugger.resource.Resource;
-import com.huskygames.rekhid.actor.StickMan;
-import com.huskygames.rekhid.slugger.GamePanel;
-import com.huskygames.rekhid.slugger.actor.AI.FsmProf;
-import com.huskygames.rekhid.slugger.actor.Fighter;
-import com.huskygames.rekhid.slugger.input.ControllerInput;
-import com.huskygames.rekhid.slugger.physics.PhysicsManager;
-import com.huskygames.rekhid.slugger.resource.Resource;
-import com.huskygames.rekhid.slugger.resource.ResourceManager;
-import com.huskygames.rekhid.slugger.sound.SoundThread;
-import com.huskygames.rekhid.slugger.util.DoublePair;
-import com.huskygames.rekhid.slugger.util.FileUtilities;
-import com.huskygames.rekhid.slugger.world.LevelComputers;
-import com.huskygames.rekhid.slugger.world.World;
-import net.java.games.input.Controller;
 import net.java.games.input.Component;
-import net.java.games.input.ControllerEnvironment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import sun.text.resources.cldr.sr.FormatData_sr_Latn_ME;
 
 import java.awt.*;
-import java.util.Queue;
+
 
 public class MainMenu extends Menu {
 
